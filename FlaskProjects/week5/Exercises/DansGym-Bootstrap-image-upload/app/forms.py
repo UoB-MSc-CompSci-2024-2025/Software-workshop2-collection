3996b2bc00f6e2df8ed00ce05f260853edaf2bbb
Logeshsharma <logeshsharmak@gmail.com>
2025-02-22 15:17:55 +0000

    Add week5 image upload and image load

--- FlaskProjects/week5/Exercises/DansGym-Bootstrap-image-upload/app/forms.py
+++ FlaskProjects/week5/Exercises/DansGym-Bootstrap-image-upload/app/forms.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.fields import StringField, PasswordField, SubmitField, IntegerField, DateField, SelectField
 from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
 
@@ -11,7 +12,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     height = IntegerField('Height', validators=[DataRequired()])
-    dob = DateField('DoB', validators=[DataRequired()], format="%Y %m %d")
+    # dob = DateField('DoB', validators=[DataRequired()], format="%Y %m %d")
     level = SelectField('Level', choices=[('Premium', 'Premium'), ('Cheap', 'Cheap')])
     submit = SubmitField('Submit', validators=[DataRequired()])
 
@@ -22,3 +23,11 @@ class RegistrationForm(FlaskForm):
     def validate_dob(self, dob):
         if dob.data < datetime(2015, 12, 1).date():
             raise ValidationError('You are too old')
+
+
+
+class ImageUploadForm(FlaskForm):
+    # There should be upload image or file action field
+    # There should be a submit button
+    image = FileField('Pick your profile picture', validators=[FileRequired(), FileAllowed(['png', 'jpg', 'jpeg'])])
+    submit = SubmitField('Upload', validators=[DataRequired()])
--- FlaskProjects/week5/Exercises/DansGym-Bootstrap-image-upload/app/views.py
+++ FlaskProjects/week5/Exercises/DansGym-Bootstrap-image-upload/app/views.py
@@ -1,6 +1,12 @@
-from flask import render_template, url_for, redirect, request, flash
+import os.path
+from crypt import methods
+from uuid import uuid4
+
+from flask import render_template, url_for, redirect, request, flash, send_from_directory
+from werkzeug.utils import secure_filename
+
 from app import app
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, ImageUploadForm
 
 
 @app.route('/')
@@ -13,7 +19,8 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'You have successfully created an account for username: {form.username.data}', 'success')
-        return redirect(url_for('registration_confirmed', username=form.username.data, email=form.email.data, level=form.level.data))
+        return redirect(url_for('registration_confirmed', username=form.username.data, email=form.email.data,
+                                level=form.level.data))
     return render_template('register.html', title='Register', form=form)
 
 
@@ -22,7 +29,8 @@ def register2():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'You have successfully created an account for username: {form.username.data}', 'success')
-        return redirect(url_for('registration_confirmed', username=form.username.data, email=form.email.data, level=form.level.data))
+        return redirect(url_for('registration_confirmed', username=form.username.data, email=form.email.data,
+                                level=form.level.data))
     return render_template('register2.html', title='Register2', form=form)
 
 
@@ -31,7 +39,8 @@ def register3():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'You have successfully created an account for username: {form.username.data}', 'success')
-        return redirect(url_for('registration_confirmed', username=form.username.data, email=form.email.data, level=form.level.data))
+        return redirect(url_for('registration_confirmed', username=form.username.data, email=form.email.data,
+                                level=form.level.data))
     return render_template('register3.html', title='Register3', form=form)
 
 
@@ -43,3 +52,21 @@ def registration_confirmed():
     return render_template('reg_confirmed.html', username=username, email=email, level=level)
 
 
+@app.route('/upload_image', methods = ['GET','POST'])
+def upload_image():
+    form = ImageUploadForm()
+    if form.validate_on_submit():
+        if form.image.data:
+            unique_str = str(uuid4())
+            filename = secure_filename(f'{unique_str}-{form.image.data.filename}')
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            form.image.data.save(filepath)
+            flash('File uploaded', 'success')
+            return render_template('upload_complete.html', title='Upload_complete', user_image=url_for('view_user_image', name=filename))
+    return render_template('upload_image.html',title ="Please upload an image", form=form)
+
+
+
+@app.route('/uploads/<name>')
+def view_user_image(name):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
